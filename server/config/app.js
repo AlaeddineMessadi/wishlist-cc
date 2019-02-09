cc3a8eed25a899da7e77a3ea65d740e54cae822d
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2019-02-09 21:20:45 +0100

    Setup server + logger + mongodb connection

---
+++ server/config/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const compress = require('compression');
+const cors = require('cors');
+const helmet = require('helmet');
+
+
+/**
+* Express instance
+* @public
+*/
+const app = express();
+
+// request logging. dev: console | production: file
+app.use(morgan(logs));
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// gzip compression
+app.use(compress());
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+module.exports = app;
