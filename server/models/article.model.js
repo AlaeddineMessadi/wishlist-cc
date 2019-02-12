95e8ff69f5be097d4c57c1ede0559b2b846f293b
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2019-02-12 19:01:01 +0100

    Create Article Model and set up routes

---
+++ server/models/article.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+let ArticleSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  company: String
+}, {
+    timestamps: true
+  });
+
+ArticleSchema.methods.toWeb = function () {
+  let json = this.toJSON();
+  json.id = this._id; //this is for the front end
+  return json;
+};
+
+module.exports = mongoose.model('Articles', ArticleSchema);
