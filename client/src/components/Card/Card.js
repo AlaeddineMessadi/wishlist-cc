09c628d500e25c5f9f20db4fc0674f879b1a905f
Alaeddine Messadi <alaeddine.messadi@icloud.com>
2019-02-13 16:13:24 +0100

    Implement search

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
 import classes from './Card.module.scss';
-const Card = () => {
+
+const defautlImage = 'https://s-media-cache-ak0.pinimg.com/736x/49/80/6f/49806f3f1c7483093855ebca1b8ae2c4.jpg';
+
+const Card = ({name,imgUrl ,price, salePrice, reviewCount, reviewRatings, subTitle}) => {
   return (
     <div className={ classes.wrapper }>
       <div className={ classes.container }>
-        <div className={ classes.top }></div>
+        <div 
+        className={ classes.top }
+        style={
+          {
+            background: `url(${imgUrl||defautlImage}) no-repeat center center`
+          }
+        }
+        ></div>
         <div className={ classes.bottom }>
           <div className={ classes.left }>
             <div className={ classes.details }>
-              <h1>Chair</h1>
-              <p>£250</p>
+              <h2>{name}</h2>
+              <p>{salePrice}</p>
             </div>
             <div className={ classes.buy }><i className={ classes.material_icons }>add_shopping_cart</i></div>
           </div>
           <div className={ classes.right }>
             <div className={ classes.done }><i className={ classes.material_icons }>done</i></div>
             <div className={ classes.details }>
-              <h1>Chair</h1>
+              <h2>{name}</h2>
               <p>Added to your cart</p>
             </div>
             <div className={ classes.remove }><i className={ classes.material_icons }>clear</i></div>
@@ -27,7 +37,11 @@ const Card = () => {
       <div className={ classes.inside }>
         <div className={ classes.icon }><i className={ classes.material_icons }>info_outline</i></div>
         <div className={ classes.contents }>
-          <p>Some description here</p>
+          <p>Name: {name}</p>
+          <p>Price: {price}</p>
+          <p>SalePrice: {salePrice}</p>
+          <p>Reviews: {reviewCount}</p>
+          <p>Rating: {reviewRatings}</p>
         </div>
       </div>
     </div>
--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -3,39 +3,42 @@ import React, { Component } from 'react';
 import classes from './Search.module.scss';
 class Search extends Component {
     state = {
-        keyword: ""
+        keyword: "",
+        products: []
     }
 
     inputHandler = (e) => {
         this.setState({ [e.target.name]: e.target.value })
-        
-        if(this.state.keyword.length >= 3) {
-            fetch('https://www.adidas.co.uk/api/search/suggestions/'+this.state.keyword)
-            .then((response)=> {
-                response.json(
-            })
-            .then((myJson) => {
-                console.log(JSON.stringify(myJson));
-            });
+
+        if (this.state.keyword.length >= 3) {
+            fetch('https://www.adidas.co.uk/api/search/suggestions/' + this.state.keyword)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((myJson) => {
+                    this.props.addToList(myJson.products);
+                });
         }
     }
 
     render() {
         return (
             <div className={classes.wrap}>
-            
+
                 <form action="#" autoComplete="on">
                     <input
                         className={classes.search}
+                        name="keyword"
                         type="text"
                         placeholder="What're we looking for ?"
-                        onChange={this.inputHandler} />
+                        onChange={(e) => { this.inputHandler(e) }} />
                     <button
                         className={classes.submit}
                         type="submit"
                         onClick={(e) => { this.props.onClick() }}
                     >Search</button>
                 </form>
+
             </div>
         )
     }
