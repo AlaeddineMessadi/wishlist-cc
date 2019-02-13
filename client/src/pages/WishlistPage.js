568f5c0d3b4843fc5b3b16ddea441f551b4831f6
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2019-02-14 00:09:26 +0100

    change to wishList page and update header and search componenets

---
+++ client/src/pages/WishlistPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import Card from '../components/Card/Card';
+
+
+class WishlistPage extends Component {
+
+  state = {
+    wishlist: [<Card />,
+    <Card />,
+    <Card />,
+    <Card />]
+  }
+  render() {
+    return (
+      <div>
+        <h1>Wishlist</h1>
+        <section>
+          {
+            this.state.wishlist.map((item, index) => (item))
+          }
+        </section>
+
+      </div>
+    )
+  }
+}
+
+export default WishlistPage;
