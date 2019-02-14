537b7564d92e69b2ce702fae94c1afd07164ba3f
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2019-02-14 16:13:24 +0100

    Implement search

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 
 import Search from '../components/Search/Search';
+import Card from '../components/Card/Card';
 
 class SearchPage extends Component {
     state = {
         list: []
     }
 
-    addToLIst = (product) => {
+    addToLIst = (products) => {
         this.setState({
-            list: [...this.state.list, product]
+            list: products
         })
+
+        console.log(this.state)
     }
     render() {
         return (
             <div>
                 <h1>Search Articles</h1>
-                <Search onChange={this.addToLIst} />
+                <Search addToList={this.addToLIst} />
+                <br/>
                 <div>
                     {
-                        this.state.list.map((i,v)=> (<p>{v}</p>))
+                        this.state.list.map((item, index) => (<Card key={index}
+                            name={item.displayName}
+                            price={item.price}
+                            reviews={item.reviewRating}
+                            imgUrl={item.imageURL}
+                        />))
                     }
                 </div>
             </div>
