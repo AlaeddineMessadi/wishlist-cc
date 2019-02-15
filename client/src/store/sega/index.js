0fb7e90e373f1a4b2510545f335d7019ef48544f
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2019-02-15 17:10:33 +0100

    update wishlist model

--- client/src/store/sega/index.js
+++ client/src/store/sega/index.js
@@ -8,11 +8,12 @@ import { wishlistService } from '../services/wishlist.service';
 
 function* createWishlistSegaAction(action) {
     try {
-        const data = yield wishlistService.createWishlist(action.payload.name);
-        console.log(data)
+        const response = yield wishlistService.createWishlist(action.payload.name);
+
+        sessionStorage.setItem('wishlist_id',response.data.wishlist.id)
         yield put(actions.successAction());
     } catch (error) {
-        yield put(actions.failedAction(error.response.data));
+        yield put(actions.failedAction(error));
     }
 }
 
@@ -46,11 +47,7 @@ function* searchArticleSegaAction(action) {
 
 function* createWishlistWatcher() {
     yield takeLatest(actionType.CREATE_WISHLIST, createWishlistSegaAction);
-}
-function* createArticleWatcher() {
     yield takeLatest(actionType.CREATE_ARTICLE, createArticleSegaAction);
-}
-function* searchArticleWatcher() {
     yield takeLatest(actionType.SEARCH_ARTICLE, searchArticleSegaAction);
 }
 
@@ -58,8 +55,6 @@ function* searchArticleWatcher() {
 
 export default function* rootSaga() {
     yield all([
-        createWishlistWatcher(),
-        createArticleWatcher(),
-        searchArticleWatcher()
+        createWishlistWatcher()
     ]);
 }
