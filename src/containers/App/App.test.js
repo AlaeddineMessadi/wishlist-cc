d66247afb6c4f11522749df886029acdefc72e66
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2019-02-16 03:48:25 +0100

    implement test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -5,16 +5,43 @@ import ReactDOM from 'react-dom';
 import { createStore } from '../../store/index';
 import App from './App';
 
+import { expect } from 'chai';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 const store = createStore();
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render((
-    <Provider store={ store }>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  ), div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App>', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render((
+      <Provider store={ store }>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    ), div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // it('Find element', () => {
+  //   const component = (<Provider store={ store }>
+  //     <BrowserRouter>
+  //       <App />
+  //     </BrowserRouter>
+  //   </Provider>)
+
+  //   const wrapper = shallow(component);
+  //   const wrapper2 = mount(component);
+
+  //   console.log(wrapper2.debug())
+
+  //   expect(wrapper.find('.search_link')).to.have.lengthOf(1);
+  // });
+
+
 });
+
+
